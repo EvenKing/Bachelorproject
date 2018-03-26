5896734c08fbca99a7ec9b2ee2dd75ae349487b3
Even <evva-gangsta@hotmail.com>
2018-03-26 15:01:00 +0200

    Create Config.php

---
+++ PHP-files/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("svr_name", <ip-adressen til databaseserver>);
+define("svr_user", <brukernavn til databaseserver>);
+define("svr_pass", <passord til databaseserver>);
+define("db_name", <navnet på databasen som brukes>);
+define("svr_port", <portnummeret til serveren>);
+
+?>
