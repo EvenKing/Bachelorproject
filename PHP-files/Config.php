216403d84b1f9cd65f4b97149c7eafdd65c4ea15
Even <evva-gangsta@hotmail.com>
2018-03-26 15:00:06 +0200

    Delete Config.php

--- PHP-files/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("svr_name", "94.139.69.2");
-define("svr_user", "soc-app");
-define("svr_pass", "!SocSecurity17");
-define("db_name", "securityonion_db");
-define("svr_port", 33066);
-
-?>
